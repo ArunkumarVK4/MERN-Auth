2bed8122971f1cea18206f5bcc19f10158b2ca86
ArunkumarVK <vkarunkumar446@gmail.com>
2023-09-15 01:41:40 +0530

    update generate token file

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,14 +4,17 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   });
-  // console.log(token)
+  console.log(token); // Log the generated token
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: true, 
-    sameSite: 'strict', 
-    maxAge: 30 * 24 * 60 * 60 * 1000, 
+    secure: true,
+    sameSite: 'strict',
+    maxAge: 30 * 24 * 60 * 60 * 1000,
   });
+
+  // Log the cookies for debugging
+  console.log('Cookies:', res.cookies);
 };
 
 export default generateToken;
