996ca46a22ba3ef1bb49d2e58a9d4b2b6d2b7fe7
Arunkumar VK <118990145+ArunkumarVK4@users.noreply.github.com>
2023-09-14 22:17:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,9 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(cors({
-  credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
